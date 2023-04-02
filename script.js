63d0567a25a4e3e1c7f924db7f36f1be964db5f1
damiro <hello.damiro@gmail.com>
2023-04-02 10:32:14 +0800

    polished add book panel

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const closeAddBook = document.querySelector('#close-add-book');
-const openAddBook = document.querySelector('#open-add-book');
-const curtain = document.querySelector('.curtain');
+const closeAddBookPanel = document.querySelector('#close-add-book');
+const openAddBookPanel = document.querySelector('#open-add-book');
+const addBookPanel = document.querySelector('.add-book-curtain');
 
-openAddBook.addEventListener('click', () => {
-    curtain.classList.toggle('hidden');
+openAddBookPanel.addEventListener('click', () => {
+    addBookPanel.classList.toggle('hidden');
 });
 
-closeAddBook.addEventListener('click', () => {
-    curtain.classList.toggle('hidden');
+closeAddBookPanel.addEventListener('click', () => {
+    addBookPanel.classList.toggle('hidden');
 });
 
-let myLibrary = [
-    { title: 'To Kill a Mockingbird', author: 'Harper Lee', percent: '100' },
-    { title: '1984', author: 'George Orwell', percent: '65' },
-    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', percent: '36' },
-    { title: 'Pride and Prejudice', author: 'Jane Austen', percent: '23' },
-    { title: 'One Hundred Years of Solitude', author: 'Gabriel García Márquez', percent: '76' },
-    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', percent: '75' },
-    { title: 'Animal Farm', author: 'George Orwell', percent: '27' },
-    { title: 'Brave New World', author: 'Aldous Huxley', percent: '93' },
-    { title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', percent: '48' },
-    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', percent: '63' },
-];
+const submitBtn = document.querySelector('#submit');
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+const books = document.querySelectorAll('.book');
+books.forEach((book) => {
+    book.addEventListener('mouseover', () => {
+        const trash = book.querySelectorAll('.comments img');
+        trash.classList.add('hidden');
+    });
+});
 
 function createBookEntry(title, author, percent) {
     const mainDiv = document.getElementsByTagName('main')[0];
@@ -58,12 +59,29 @@ function createBookEntry(title, author, percent) {
     trashDiv.setAttribute('class', 'btn');
 }
 
+let myLibrary = [
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', percent: '100' },
+    { title: '1984', author: 'George Orwell', percent: '65' },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', percent: '36' },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', percent: '23' },
+    { title: 'One Hundred Years of Solitude', author: 'Gabriel García Márquez', percent: '76' },
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', percent: '75' },
+    { title: 'Animal Farm', author: 'George Orwell', percent: '27' },
+    { title: 'Brave New World', author: 'Aldous Huxley', percent: '93' },
+    { title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', percent: '48' },
+    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', percent: '63' },
+];
+
+function renderLibrary() {
+    for (let i = 0; i <= myLibrary.length; i++) {
+        createBookEntry(myLibrary[i].title, myLibrary[i].author, myLibrary[i].percent);
+    }
+}
+
 function Book(title, author, percent) {
     this.title = title;
     this.author = author;
     this.percent = percent;
 }
 
-for (let i = 0; i <= myLibrary.length; i++) {
-    createBookEntry(myLibrary[i].title, myLibrary[i].author, myLibrary[i].percent);
-}
+renderLibrary();
